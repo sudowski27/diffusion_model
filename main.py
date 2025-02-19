4257a72637c9f3734b7bc20b06b1196b2a4fce8b
sudowski27 <Sudowski@o2.pl>
2025-02-19 01:28:12 +0100

    Update file main.py

--- main.py
+++ main.py
@@ -38,6 +38,28 @@ def main():
         param_1,
         number_of_diffrent_sets
     )
+
+    data_2, data_debug_2 = get_dataset(
+        number_of_samples,
+        param_2,
+        number_of_diffrent_sets
+    )
+    data_3, data_debug_3 = get_dataset(
+        number_of_samples,
+        param_3,
+        number_of_diffrent_sets
+    )
+    data_4, data_debug_4 = get_dataset(
+        number_of_samples,
+        param_4,
+        number_of_diffrent_sets
+    )
+    data_5, data_debug_5 = get_dataset(
+        number_of_samples,
+        param_5,
+        number_of_diffrent_sets
+    )
+
     for db_data in data_debug:
         plt.plot(db_data, 'b-', linewidth=2.5)
     plt.savefig(plot_0_fname)
@@ -50,8 +72,16 @@ def main():
     layer_widths_decoder = [101, 150, 300, 600, 1200, 2400]
     param_tensor_0 = torch.tensor([param], dtype=torch.float32, device=device)
     param_tensor_1 = torch.tensor([param_1], dtype=torch.float32, device=device)
+    param_tensor_2 = torch.tensor([param_2], dtype=torch.float32, device=device)
+    param_tensor_3 = torch.tensor([param_3], dtype=torch.float32, device=device)
+    param_tensor_4 = torch.tensor([param_4], dtype=torch.float32, device=device)
+    param_tensor_5 = torch.tensor([param_5], dtype=torch.float32, device=device)
     data_tensor_0 = torch.tensor(data, dtype=torch.float32, device=device)
     data_tensor_1 = torch.tensor(data_1, dtype=torch.float32, device=device)
+    data_tensor_2 = torch.tensor(data_2, dtype=torch.float32, device=device)
+    data_tensor_3 = torch.tensor(data_3, dtype=torch.float32, device=device)
+    data_tensor_4 = torch.tensor(data_4, dtype=torch.float32, device=device)
+    data_tensor_5 = torch.tensor(data_5, dtype=torch.float32, device=device)
 
     plt.plot(data)
     plt.savefig(plot_1_fname)
@@ -71,15 +101,31 @@ def main():
     global_loss = []
     FIRST_DATA = 0
     SECOND_DATA = 1
+    THIRD_DATA = 2
+    FOURTH_DATA = 3
+    FIFTH_DATA = 4
+    SIXTH_DATA = 5
     for epoch in range(epoch_offset, epoch_offset + epoch_num):
         i = int(np.random.uniform(0, 40))
-        param_choice = np.random.randint(0, 2)
+        param_choice = np.random.randint(0, 6)
         if param_choice == FIRST_DATA:
             data_tensor = data_tensor_0
             param_tensor = param_tensor_0
         elif param_choice == SECOND_DATA:
             data_tensor = data_tensor_1
             param_tensor = param_tensor_1
+        elif param_choice == THIRD_DATA:
+            data_tensor = data_tensor_2
+            param_tensor = param_tensor_2
+        elif param_choice == FOURTH_DATA:
+            data_tensor = data_tensor_3
+            param_tensor = param_tensor_3
+        elif param_choice == FIFTH_DATA:
+            data_tensor = data_tensor_4
+            param_tensor = param_tensor_4
+        elif param_choice == SIXTH_DATA:
+            data_tensor = data_tensor_5
+            param_tensor = param_tensor_5
 
         noise = torch.randn_like(data_tensor, device=device)
 
@@ -104,10 +150,14 @@ def main():
 
         mu, std = mlp_model(xt, i, param_tensor)
 
+        mse_error = torch.nn.MSELoss()(
+            xt, mu + epsilon * std
+        )
+
         kl = torch.log(std) - torch.log(std_q) + (
             std_q**2 + (mu_q - mu)**2) / (2 * std**2)
         k = - kl.mean()
-        loss = -k
+        loss = -k + mse_error * 2
 
         optimizer.zero_grad()
         loss.backward()
@@ -136,6 +186,30 @@ def main():
                     param_tensor_1,
                     timesteps - 1
                 ).cpu().numpy()
+                data_np2 = get_data_from_noise(
+                    noise,
+                    mlp_model,
+                    param_tensor_2,
+                    timesteps - 1
+                ).cpu().numpy()
+                data_np3 = get_data_from_noise(
+                    noise,
+                    mlp_model,
+                    param_tensor_3,
+                    timesteps - 1
+                ).cpu().numpy()
+                data_np4 = get_data_from_noise(
+                    noise,
+                    mlp_model,
+                    param_tensor_4,
+                    timesteps - 1
+                ).cpu().numpy()
+                data_np5 = get_data_from_noise(
+                    noise,
+                    mlp_model,
+                    param_tensor_5,
+                    timesteps - 1
+                ).cpu().numpy()
             plt.plot(noise_np)
             plt.savefig(f"results/noise/{epoch}")
             plt.close()
@@ -145,10 +219,30 @@ def main():
             plt.plot(data_np1)
             plt.savefig(f"results/data/{epoch}_param1.png")
             plt.close()
+            plt.plot(data_np2)
+            plt.savefig(f"results/data/{epoch}_param2.png")
+            plt.close()
+            plt.plot(data_np3)
+            plt.savefig(f"results/data/{epoch}_param3.png")
+            plt.close()
+            plt.plot(data_np4)
+            plt.savefig(f"results/data/{epoch}_param4.png")
+            plt.close()
+            plt.plot(data_np5)
+            plt.savefig(f"results/data/{epoch}_param5.png")
+            plt.close()
 
             plt.plot(np.abs(data_np0 - noise_np))
             plt.savefig(f"results/abs/{epoch}")
             plt.close()
+
+            checkpoint = {
+                'epoch': epoch,  # Aktualna epoka
+                'model_state_dict': mlp_model.state_dict(),  # Wagi modelu
+                'optimizer_state_dict': optimizer.state_dict(),  # Stan optymalizatora
+                'loss': loss,  # Ostatnia wartość funkcji kosztu
+            }
+            torch.save(checkpoint, "checkpoint.pth")
 
     plt.plot(global_loss)
     plt.show()
